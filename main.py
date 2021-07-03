4f9b27a62b7f847758e8eebd65b458789636c33c
kastrahl <56175823+kastrahl@users.noreply.github.com>
2021-07-03 14:21:48 +0000

    quote api and inpire command works now

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-import discord 
+import discord
 import os
+import requests
+import json
+
 my_secret = os.environ['TOKEN']
+
+
+def get_quotes():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " ~" + json_data[0]['a']
+    return (quote)
 
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-  print('We have logged in as {0,user}'.format(client))
+    print('We have logged in as {0,user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello !')
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello !')
+
+    if message.content.startswith('$inspire'):
+        quote = get_quotes()
+        await message.channel.send(quote)
+
 
 client.run(my_secret)
